2f089ca481cc419281b5b4a95331d1f961969910
Raphib737 <raph737@bu.edu>
2015-10-26 19:57:48 -0400

    Weather section Added

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-// var xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function() {
-//     if (xhttp.readyState == 4 && xhttp.status == 200) {
-//       document.getElementById("espn").innerHTML = xhttp.responseText;
-//     }
-//   }
-//   xhttp.open("GET", "api.espn.com/v1/sports/news/headlines/top", true);
-//   xhttp.send();
 
 
 
-
-
-$http({
-  method: 'GET',
-  url: 'api.espn.com/v1/sports/news/headlines/top'
-}).then(function successCallback(response) {
-    // this callback will be called asynchronously
-    // when the response is available
-  }, function errorCallback(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
+$("input").on("keydown",function search(e) {
+    if(e.keyCode == 13) { 
+        var location = $(this).val().toLowerCase();
+        var url = "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='".concat(location,"')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+		$.get(url, function(data) {
+	  		//alert( "Data Loaded: " + data );
+		  	// var json = data;
+		  	// console.log(json);
+		  	// console.log(json["query"]["results"]["channel"]);
+		  	var dayforecast = data["query"]["results"]["channel"]["item"]["forecast"];
+		  	var table = document.getElementById("tbody");
+		  	document.getElementById("tbody").innerHTML="";
+		  	for (var i = 0; i<dayforecast.length; i++) {
+		  		var row = table.insertRow(-1);
+				row.insertCell(0).innerHTML = dayforecast[i]['date'];
+				row.insertCell(1).innerHTML = dayforecast[i]['day'];
+				row.insertCell(2).innerHTML = dayforecast[i]['text'];
+				row.insertCell(3).innerHTML = dayforecast[i]['high'];
+				row.insertCell(4).innerHTML = dayforecast[i]['low'];
+		  };
+		});
+	}});
