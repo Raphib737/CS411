4423d446caa4668720b7fe2b98e1a18cb94302cc
Raphib737 <raph737@bu.edu>
2015-11-17 13:13:11 -0500

    updated data

--- js/main.js
+++ js/main.js
@@ -2,19 +2,17 @@
 
 
 $("input").on("keydown",function search(e) {
-    if(e.keyCode == 13) { 
-        var location = $(this).val().toLowerCase();
+    if(e.keyCode == 13) { //Enter Key
+        var location = $(this).val().toLowerCase(); //Grabs Location string from Search Bar
         var url = "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='".concat(location,"')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
 		$.get(url, function(data) {
-	  		//alert( "Data Loaded: " + data );
-		  	// var json = data;
-		  	// console.log(json);
-		  	// console.log(json["query"]["results"]["channel"]);
+			//Parsing data and removing past data 
 		  	var dayforecast = data["query"]["results"]["channel"]["item"]["forecast"];
-		  	var table = document.getElementById("tbody");
+		  	var table = document.getElementById("tbody"); 
 		  	document.getElementById("tbody").innerHTML="";
 		  	document.getElementById("location").innerHTML=data["query"]["results"]["channel"]["description"]
-		  	for (var i = 0; i<dayforecast.length; i++) {
+
+		  	for (var i = 0; i<dayforecast.length; i++) { //Append 5 day forecast onto the website
 		  		var row = table.insertRow(-1);
 				row.insertCell(0).innerHTML = dayforecast[i]['date'];
 				row.insertCell(1).innerHTML = dayforecast[i]['day'];
@@ -24,3 +22,16 @@ $("input").on("keydown",function search(e) {
 		  };
 		});
 	}});
+
+
+
+
+
+var url="http://api.nytimes.com/svc/topstories/v1/technology.json?api-key=aa20e34fa50838b1e142474106eb1598:14:73335924";
+$.get(url, function(data){
+	console.log(data);
+	for(var x =0; x<30;x++){
+		console.log(data['results'][x]["title"]+"\n"); 
+		console.log("\t\t"+data['results'][x]["abstract"]);
+	}
+});
